d345b9d3e8f782622b1c811b9489eb1f5f452f1a
Zeebuhh <ngroth277@gmail.com>
2024-11-02 17:37:24 +0100

    Extract method lowercaseTags

--- webshop/src/main/java/com/example/webshop/repository/ProductRepository.java
+++ webshop/src/main/java/com/example/webshop/repository/ProductRepository.java
@@ -8,29 +8,31 @@ import java.util.List;
 
 public class ProductRepository {
 
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "AMD Ryzen",
+                    "aoifwoiafw aowifh wwfwewef",
+                    79900,
+                    Arrays.asList("AMD", "processor")
+            ),
+            new ProductResponse(
+                    "2",
+                    "Intel Core i-9",
+                    "aoifwoiafw aowifh wwfwewef",
+                    50900,
+                    Arrays.asList("Intel", "processor")
+            ),
+            new ProductResponse(
+                    "3",
+                    "NVIDIA GeForce",
+                    " wa d wdaA Woifwoiafw aowifh awdawdwwawdawdfwewef",
+                    27900,
+                    Arrays.asList("NVIDIA", "graphics")
+            ));
+
     public List<ProductResponse> findAll(String tag){
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "AMD Ryzen",
-                        "aoifwoiafw aowifh wwfwewef",
-                        79900,
-                        Arrays.asList("AMD", "processor")
-                ),
-                new ProductResponse(
-                        "2",
-                        "Intel Core i-9",
-                        "aoifwoiafw aowifh wwfwewef",
-                        50900,
-                        Arrays.asList("Intel", "processor")
-                ),
-                new ProductResponse(
-                        "3",
-                        "NVIDIA GeForce",
-                        " wa d wdaA Woifwoiafw aowifh awdawdwwawdawdfwewef",
-                        27900,
-                        Arrays.asList("NVIDIA", "graphics")
-                ));
+
 
         if(tag == null)
             return products;
@@ -38,13 +40,7 @@ public class ProductRepository {
             List<ProductResponse> filtered = new ArrayList<>();
             for (ProductResponse p: products){
 
-                List<String> lowercaseTags = new ArrayList<>();
-
-                for(String t: p.getTags()){
-                    lowercaseTags.add(t.toLowerCase());
-                }
-
-                if (lowercaseTags.contains(tag.toLowerCase())) {
+                if (lowercaseTags(p).contains(tag.toLowerCase())) {
                     filtered.add(p);
                 }
             }
@@ -52,5 +48,17 @@ public class ProductRepository {
 
         }
     }
+
+
+    private List<String> lowercaseTags(ProductResponse p){
+        List<String> lowercaseTags = new ArrayList<>();
+
+        for(String t: p.getTags()){
+            lowercaseTags.add(t.toLowerCase());
+        }
+
+        return lowercaseTags;
+    }
+
 
 }
